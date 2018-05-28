035bbc2cd299b31d8b3d56dccaa6b4ea7f0f2064
Mudith Chathuranga <mudithchathuranga@gmail.com>
2018-05-28 17:22:14 +0530

    Initial Commit

---
+++ AppDataPlist.swift
@@ -0,0 +1,51 @@
+//
+//  AppDataPlist.swift
+//  StackTest
+//
+//  Created by Mudith Chathuranga on 5/28/18.
+//  Copyright © 2018 Chathuranga. All rights reserved.
+//
+
+import Foundation
+
+class AppDataPlist {
+    
+    static let defaults = UserDefaults.standard
+    
+    static func archiveUserInformation<T : Codable>(info: T) {
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .xml
+        do {
+            let data = try encoder.encode(info)
+            try data.write(to: self.getPath())
+        } catch {
+            // Handle error
+            print(error)
+        }
+    }
+    
+    static func saveUserInformation<T : Codable>(info: T, key: UserData.RawValue) {
+        self.archiveUserInformation(info: info)
+    }
+    
+    static func retrieveUserInformation<T : Codable>(key: UserData.RawValue, type: T.Type) -> Any {
+        if let data = try? Data(contentsOf: self.getPath()) {
+            let decoder = PropertyListDecoder()
+            return try! decoder.decode(T.self, from: data)
+        } else {
+            return ""
+        }
+    }
+    
+    
+    //Get Path
+    static func getPath() -> URL {
+        let plistFileName = "Data.plist" // plist name
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentPath = paths[0] as NSString
+        let plistPath = documentPath.appendingPathComponent(plistFileName)
+        return URL(fileURLWithPath: plistPath)
+    }
+    
+}
+
